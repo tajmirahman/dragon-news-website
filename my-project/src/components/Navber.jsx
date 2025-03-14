bf0eb5ec9c02962b063077ecf84ba5278b96c726
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-14 16:43:56 +0600

    navber page design done

--- my-project/src/components/Navber.jsx
+++ my-project/src/components/Navber.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
+import userLogo from '../assets/user.png'
 
 const Navber = () => {
     return (
         <div className='flex justify-between items-center'>
 
             <div></div>
-            <div>
+
+            <div className='space-x-3'>
                 <Link to={'/'}>Home</Link>
                 <Link to={'/about'}>About</Link>
                 <Link to={'/carrer'}>Carrer</Link>
             </div>
-            <div></div>
+
+            <div className='flex justify-center items-center gap-2'>
+                <img src={userLogo} alt="" />
+                <button className='btn bg-[#403F3F] text-white'>Login</button>
+            </div>
             
         </div>
     );
