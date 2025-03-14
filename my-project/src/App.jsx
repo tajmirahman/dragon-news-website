d75c29a44304b1cfdcd70cd81251439c8da04801
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-14 14:47:08 +0600

    dragon news start

---
+++ my-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='bg-red-700 text-6xl'>Vite + React</h1>
+      <button className='btn btn-warning'>hi</button>
+      
+    </>
+  )
+}
+
+export default App
