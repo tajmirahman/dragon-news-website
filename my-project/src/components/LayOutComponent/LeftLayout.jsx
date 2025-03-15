7305d87863b36b6ccd181a1ba19c473d181c86cf
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-15 23:41:38 +0600

    all category map done

--- my-project/src/components/LayOutComponent/LeftLayout.jsx
+++ my-project/src/components/LayOutComponent/LeftLayout.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
 const LeftLayout = () => {
-    const [category,setCategory]=useState([]);
+    const [categorires, setCategories] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("https://openapi.programming-hero.com/api/news/categories")
-        .then(res=>res.json())
-        .then(data=>setCategory(data.data.news_category
-        ))
-    },[])
+            .then(res => res.json())
+            .then(data => setCategories(data.data.news_category
+            ))
+    }, [])
 
     return (
         <div>
-            <h1 className='font-semibold'>All Category {category.length}</h1>
+            <h1 className='font-semibold'>All Category {categorires.length}</h1>
+
+            <div>
+                {
+                    categorires.map(category=> <button key={category.category_id}>{category.category_name}</button>)
+                }
+            </div>
         </div>
     );
 };
