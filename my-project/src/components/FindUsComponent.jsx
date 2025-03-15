683b549a6e180019fef3d3f501cef0f4e2a14bb9
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-16 00:16:24 +0600

    find us component start

---
+++ my-project/src/components/FindUsComponent.jsx
@@ -0,0 +1,17 @@
+
+
+const FindUsComponent = () => {
+    return (
+        <div>
+            <h2 className="font-semibold mb-3">Find Us On</h2>
+
+            <div className="join join-vertical w-full space-y-3">
+                <button className="btn join-item justify-start">Button</button>
+                <button className="btn join-item justify-start">Button</button>
+                <button className="btn join-item justify-start">Button</button>
+            </div>
+        </div>
+    );
+};
+
+export default FindUsComponent;
--- my-project/src/components/LayOutComponent/RightNav.jsx
+++ my-project/src/components/LayOutComponent/RightNav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SocialComponent from '../SocialComponent';
+import FindUsComponent from '../FindUsComponent';
 
 const RightNav = () => {
     return (
-        <div>
+        <div className='space-y-5'>
             <SocialComponent />
+            <FindUsComponent />
         </div>
     );
 };
