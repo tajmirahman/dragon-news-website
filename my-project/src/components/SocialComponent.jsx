b8299243289b6b11a6c000e827fc80b9ebf8649a
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-15 23:56:41 +0600

    react icons install done

--- my-project/src/components/SocialComponent.jsx
+++ my-project/src/components/SocialComponent.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const SocialComponent = () => {
     return (
         <div>
             <h1>Login With</h1>
             <div>
-                <button className="btn"></button>
-                <button className="btn"></button>
+                <button className="btn"><FaGoogle />Login With Google</button>
+                <button className="btn"><FaGithub />Login With Github</button>
             </div>
         </div>
     );
