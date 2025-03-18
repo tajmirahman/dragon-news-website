b17b6204b0eb42448ca9e30a121d1e64d061c9d3
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-18 14:54:07 +0600

    login page design done

--- my-project/src/pages/Login.jsx
+++ my-project/src/pages/Login.jsx
@@ -1,24 +1,29 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
 
 
-            <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row-reverse">
 
-                <div className="card bg-base-100 w-full max-w-lg shrink-0">
-                    <div className="card-body">
+            <div className="card bg-base-100  w-full max-w-md shrink-0">
+                <h1 className="text-center text-2xl font-semibold mt-3">Login your account</h1>
+                <div className="card-body ">
+                    <form >
                         <fieldset className="fieldset">
                             <label className="fieldset-label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" className="input w-full" placeholder="Email" />
                             <label className="fieldset-label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" className="input w-full" placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Login</button>
                         </fieldset>
-                    </div>
+                        <p>Don't Have an Account? Please <Link to={'/auth/register'} className="text-red-700">Register</Link></p>
+                    </form>
                 </div>
             </div>
+        </div>
 
 
     );
