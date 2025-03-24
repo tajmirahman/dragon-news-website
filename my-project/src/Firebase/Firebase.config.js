51981cdcd7ceff516c4caae7280944ff48a379b7
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-24 23:08:39 +0600

    firebase hidden file finish successfully

--- my-project/src/Firebase/Firebase.config.js
+++ my-project/src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwhNIpCVWR8km_covTbneMErzHArklu2Q",
-  authDomain: "dragon-news-d5532.firebaseapp.com",
-  projectId: "dragon-news-d5532",
-  storageBucket: "dragon-news-d5532.firebasestorage.app",
-  messagingSenderId: "703553487290",
-  appId: "1:703553487290:web:8c9fc45ab11caecedc9d83"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
